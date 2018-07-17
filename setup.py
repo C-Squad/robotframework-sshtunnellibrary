731e7c1e4374aa1421713763a599d7e9f85230b3
Ramkumar Bangaru <ramkumar.bangaru@gmail.com>
2018-07-17 14:17:15 -0400

    Update Setup.py
    Added long description and CLASSIFIERS to the repo

--- setup.py
+++ setup.py
@@ -7,13 +7,34 @@ with open(version_file) as file:
       code = compile(file.read(), version_file, 'exec')
       exec(code)
 
+CLASSIFIERS = '''
+Development Status :: 5 - Production/Stable
+License :: OSI Approved :: Apache Software License
+Operating System :: OS Independent
+Programming Language :: Python
+Programming Language :: Python :: 2.7
+Programming Language :: Python :: 3.4
+Programming Language :: Python :: 3.5
+Programming Language :: Python :: 3.6
+Topic :: Software Development :: Testing
+Framework :: Robot Framework
+Framework :: Robot Framework :: Library
+'''.strip().splitlines()
+      
+DESCRIPTION = ""
+with open(join(CURDIR, 'README.md')) as f:
+    DESCRIPTION = f.read()
+
 setup(name='robotframework-sshtunnellibrary',
       version=VERSION,
       description='SSH Tunnel Library for Robot framework',
       author='C-Squad',
       author_email='csquad.dev@gmail.com',
       license= 'Apache-2.0',
+      long_description=DESCRIPTION,
+      url='https://github.com/C-Squad/robotframework-sshtunnellibrary',
       platforms= 'any',
+      classifiers=CLASSIFIERS,
       packages=[
             'SSHTunnelLibrary'
       ],
